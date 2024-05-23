a021352054bc0ef43853836e6c47828c6b358ac5
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-05-23 18:03:05 +0300

    Fix dbURL

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: `mongodb+srv://amir:amir123@cluster0.ctkucyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    dbURL: `mongodb+srv://amir123:amir123@cluster0.ctkucyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     dbName: 'jobTracker',
 }
