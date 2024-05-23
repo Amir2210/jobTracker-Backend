1fd36c945f5b76a06f2b7761478eb4340545e4bf
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-05-23 17:57:06 +0300

    Change for BASE_URL in frontend

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
-    // dbURL: 'mongodb+srv://amir:amir123@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    // dbURL: `mongodb+srv://amir:${process.env.SECRETMONGO}@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: `mongodb+srv://amir123:amir123@cluster0.ctkucyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     dbName: 'jobTracker',
 }
