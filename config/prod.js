85813a993744ccdbe384f51d6933eca6d6f58125
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-05-23 13:52:41 +0300

    first production

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 export default {
     // dbURL: 'mongodb+srv://amir:amir123@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    dbURL: `mongodb+srv://amir:${process.env.SECRETMONGO}@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    dbURL: `mongodb+srv://amir:${process.env.SECRETMONGO}@cluster0.ctkucyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     dbName: 'jobTracker',
 }
