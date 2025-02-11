f483736db87df1c55ecec2bd5b2dbd599259d6fe
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-02-11 21:55:34 +0200

    Add limiter request to login function

--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -1,9 +1,15 @@
 import express from 'express'
-
 import { login, signup, logout } from './auth.controller.js'
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.'
+});
 
 export const authRoutes = express.Router()
 
-authRoutes.post('/login', login)
+authRoutes.post('/login', limiter, login)
 authRoutes.post('/signup', signup)
 authRoutes.post('/logout', logout)
